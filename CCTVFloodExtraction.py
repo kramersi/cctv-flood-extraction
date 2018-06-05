17a8b6c22fd91821dbf2f9b178b9cec1bee8ddbe
kramersi <simon.kramer@jesus.ch>
2018-06-05 15:37:00 +0200

    setting up code for loading model and predict with tensorflow methods

--- CCTVFloodExtraction.py
+++ CCTVFloodExtraction.py
@@ -1,21 +1,21 @@
-import numpy
 import cv2
 import os
 from datetime import datetime
 import numpy as np
+from PIL import Image
 
 
 class CCTVFloodExtraction(object):
 
-    def __init__(self, video_path, model_path, frame_path):
-        self.video_path = video_path
-        self.model_path = model_path
-        self.frame_path = frame_path
+    def __init__(self, video_dir, model_dir, frame_path):
+        self.video_dir = video_dir
+        self.model_dir = model_dir
+        self.frame_dir = frame_dir
 
     def setup_outputfolder(self):
         # setup the output folder
         output = self.frame_path
-        path = self.video_path
+        path = self.video_dir
         if output is None:
             output = path[:-4]
         else:
@@ -57,25 +57,69 @@ class CCTVFloodExtraction(object):
             index += 1
         print('frame extracted from video')
 
-    def load_model(self, model_type='unet', channels, classes,):
-        if model_type == 'unet':
-            from models.tf_unet import unet
-            net = unet.Unet(
-                channels=self.channels'],
-                n_class=self.network['classes'],
-                layers=self.network['layers'],
-                features_root=self.network['features_root'],
-                cost_kwargs=dict(class_weights=s.network['class_weights'])
-            )
-            # Run prediction
-            net.predict_no_label(
-                model_path=os.path.join(model_dir, 'model.cpkt'),
-                images_dir=sequence_dir, output_dir=output_dir, )
+    def load_model(self, model_type='tensorflow', n_class=2):
+        """ loads neural network model of different types.
 
-        print('model_loaded')
+        The pretrained model must have following specifications:
+            Input: RGB-Picture as an array of width*height*channel
+            Classes: Has to have two classes which are labelled 0=background and 1=water
+            Output: array of width*height*channel*class_number
 
-    def run_segmentation(self):
-        print('segmentation started')
+        Args:
+            model_type (str): type of model, which should be loaded, has to be one of tensorflow, keras, caffee etc.
+            n_class (int): number of classes (defaults to two classes: background and water
+
+        """
+        def load_image(path, dtype=np.float32):
+            data = np.array(Image.open(path), dtype)
+            # normalization
+            data -= np.amin(data)
+            data /= np.amax(data)
+            return data
+
+        if model_type == 'tensorflow':
+            import tensorflow as tf
+            tensor_node_op = 'example:0'
+            tensor_node_x = 'x:0'
+            tensor_node_y = 'y:0'
+            tensor_node_prob = 'z:0'
+
+            node_names = [tensor.name for tensor in tf.get_default_graph().as_graph_def().node]
+            print(node_names)
+
+            with tf.Session() as sess:
+                saver = tf.train.import_meta_graph('my-model-1000.meta')
+                saver.restore(sess, self.model_dir)
+                print("Model restored from file: %s" % self.model_dir)
+
+                #x = tf.placeholder("float", shape=[None, None, None, channels])
+                #y = tf.placeholder("float", shape=[None, None, None, n_class])
+                #keep_prob = tf.placeholder(tf.float32)  # dropout (keep probability)
+
+                # get the graph in the current thread
+                graph = tf.get_default_graph()
+
+                # access the input key words for feed_dict
+                x = graph.get_tensor_by_name(tensor_node_x)
+                y = graph.get_tensor_by_name(tensor_node_y)
+                keep_prob = graph.get_tensor_by_name(tensor_node_prob)
+
+                # Now, access the operation that you want to run.
+                restored_op = graph.get_tensor_by_name(tensor_node_op)
+
+                # loop through files and save predictions
+                for image in os.listdir(self.images_dir):
+                    x = load_image(os.path.join(self.images_dir, image))
+                    y_dummy = np.empty((x.shape[0], x.shape[1], x.shape[2], n_class))
+                    prediction = sess.run(restored_op, feed_dict={x: x, y: y_dummy, keep_prob: 1.})[0]
+
+                    img = Image.fromarray((prediction * 200).astype(np.uint8))
+                    img.save(os.path.join(self.output_dir, os.path.splitext(image)[0] + '.png'))
+
+            if model_type == 'keras':
+                print('not implemented')
+
+        print('model loaded and images predicted')
 
     def flood_extraction(self):
         print('flood signal extracted')
