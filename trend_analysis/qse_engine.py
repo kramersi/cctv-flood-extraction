correct that delta could be defined for each derivative

trend_analysis/qse_engine.py
@@ -60,6 +60,7 @@ def stdev_estimation(signal):
     diffs = np.absolute(np.diff(signal, axis=ax))
 
     if ax == 0:
+        #return np.mean(diffs, axis=ax) / 0.9539
         non_zero_ind = np.nonzero(diffs)[0]
         if non_zero_ind.size == 0:
             return 0.0
@@ -126,10 +127,16 @@ def non_negative_validation(value):
     Validate if value is negative and raise Validation error
 
     """
-    if value <= 0:
-        raise ValueError("The Value must not be negative.")
+    if isinstance(value, list):
+        if any(v < 0 for v in value):
+            raise ValueError("The Values in the list must not be negative")
+        else:
+            return value
     else:
-        return value
+        if value < 0:
+            raise ValueError("The Value must not be negative.")
+        else:
+            return value
 
 
 def extract_primitives(transitions):
@@ -186,8 +193,8 @@ class GeneralQSE(object):
     transition between primitives.
 
     Args:
-        sigma_eps (float): pre-supposed measurement error variance, its now not presupposed but calculated with
-        standard deviation of filtered signal and raw signal.
+        sigma_eps (float, str): pre-supposed measurement error variance, if set to 'auto' its not presupposed but
+            calculated with standard deviation of filtered signal and raw signal.
         order (int): order of regression model, must be higher than 2 (2: linear, 3: quadratic, etc)
         delta (float): bandwidth parameter for the probability of zero-valued derivative. It indicates at which
             Percent of the maximum signal value, 1st and 2nd derivative the Probability to be in zero state is 68%.
@@ -295,14 +302,14 @@ class GeneralQSE(object):
 
     bw_option = dict(n_support=None, min_support=10, max_support=100, ici_span=4.4, rel_threshold=0.85, m_estimator=True)
 
-    def __init__(self, kernel='tricube', order=3, delta=0.05, transitions=allowed_trans, bw_estimation='fix',
+    def __init__(self, kernel='tricube', order=3, delta=0.05, sigma_eps='auto', transitions=allowed_trans, bw_estimation='fix',
                  bw_options=bw_option):
 
         # initialisation of kernel regression properties
         self.bw_estimation = bw_estimation
         self.bw_options = bw_options
         self.n_support = bw_options['n_support']
-        self.sigma_eps = 1
+        self.sigma_eps = sigma_eps
         self.order = order_validation(order)
         self.kernel = kernel_validation(kernel)
         self.delta = non_negative_validation(delta)
@@ -405,7 +412,7 @@ class GeneralQSE(object):
         """
         Make polynomial kernel regression by creating array of all moving windows, calculating projection matrix and
         calculating.
-        # Make signal longer at beginnig and end by the half of the delay, that filter exactly starts at first entry
+        # Make signal longer at beginning and end by the half of the delay, that filter exactly starts at first entry
         # and not half of delay later. It does so by taking the nearest value and multiplies it.
         Args:
             signal: signal to be smoothed
@@ -455,17 +462,24 @@ class GeneralQSE(object):
         sig_n = y_stacks.shape[0]
         influence_matrix = (self.regr_basis.dot(proj_matrix))
         sigmas = np.full((sig_n, self.coeff_nr), np.nan)
-
+        std_res = np.empty(sig_n)
         for i in range(sig_n):
             residuals = y_stacks.T[:, i] - np.dot(influence_matrix, y_stacks.T[:, i])
-            sigma_eps = max(np.std(residuals), min_stdev)
+
+            if self.sigma_eps == 'auto':
+                sigma_eps = max(np.std(residuals), self.precision)
+            else:
+                sigma_eps = self.sigma_eps
+
             sigma_b = (proj_matrix * sigma_eps).dot(proj_matrix.T)
             stdev_b = np.diag(sigma_b) ** (1 / 2)  # point-wise standard deviations out of diagonal covariance matrix
             stdev_b = stdev_b[0:self.coeff_nr]
 
-            sigmas[i, :] = stdev_b
+            std_res[i] = stdev_b[0]
+            #stdev_b[0] = min_stdev
+            sigmas[i, :] = stdev_b  # [std, std, std] * stdev_b / stdev_b[0]
 
-        return sigmas
+        return sigmas, std_res
 
     def local_polynom_regression(self, signal, bw, min_stdev=precision):
 
@@ -479,9 +493,9 @@ class GeneralQSE(object):
         else:
             # only if all values are not nan, but this is not the case because signal interpolated at beginning
             features = proj_matrix.dot(y_stacks.T).T
-            stdev = self.coefficient_uncertainty(proj_matrix, y_stacks, min_stdev)
+            stdev, std_res = self.coefficient_uncertainty(proj_matrix, y_stacks, min_stdev)
 
-        return features, stdev
+        return features, stdev, std_res
 
     def infer_probabilities(self, features, stdev):
         """ calculate probabilities of selected primitives out of polynom features and their standard deviation
@@ -492,24 +506,24 @@ class GeneralQSE(object):
             stdev (ndarray): standard deviation of the polynom coefficients
 
         """
-        b = features / stdev  # normalise features by their standard deviation, necessary to use norm.cdf
+        #b = features / stdev  # normalise features by their standard deviation, necessary to use norm.cdf
 
         # get the max of each feature over time and take a procentage of it, attention to second derivative.
-        deltas = np.nanmax(np.absolute(b), axis=0) * self.delta
+        deltas = np.nanmax(np.absolute(features), axis=0) * self.delta  # np.nanmax(np.absolute(b), axis=0) * [0.2, self.delta, self.delta]
 
         # if deltas.size > 2:
         #     deltas[-1] = deltas[-1]*2
         #     deltas[-2] = deltas[-2]*2
 
         # Convert regression coefficients to probabilities for qualitative state
-        prob_p = norm.logcdf(b - deltas)
-        prob_n = norm.logcdf(-b - deltas)
-        prob_pn = norm.logcdf(deltas - b)
+        prob_p = norm.logcdf((features - deltas) / (stdev))
+        prob_n = norm.logcdf((-features - deltas) / (stdev))
+        prob_pn = norm.logcdf((deltas - features) / (stdev))
         prob_0 = prob_pn + np.log1p(-np.exp(prob_n-prob_pn))
 
-        params = {'pos': prob_p, 'neg': prob_n, 'zero': prob_0, 'ignore': np.zeros(b.shape)}
+        params = {'pos': prob_p, 'neg': prob_n, 'zero': prob_0, 'ignore': np.zeros(features.shape)}
 
-        py = np.zeros((b.shape[0], self.prim_nr))
+        py = np.zeros((features.shape[0], self.prim_nr))
 
         # collect for each primitive the right probability and adds them together (assume independent probabilities)
         for i, prim in enumerate(self.primitives):  # iterate over primitives
@@ -606,7 +620,11 @@ class GeneralQSE(object):
                 maxi += 1
 
             features[i, :] = feature
-            sigma_eps = max(np.std(_w_err), d)
+
+            if self.sigma_eps == 'auto':
+                sigma_eps = max(np.std(_w_err), d)
+            else:
+                sigma_eps = self.sigma_eps
             sigma_b = (proj_matrix * sigma_eps).dot(proj_matrix.T)
             stdev_b = np.diag(sigma_b) ** (1 / 2)  # point-wise standard deviations out of diagonal covariance matrix
             stdev_b = stdev_b[0:self.coeff_nr]
@@ -684,13 +702,13 @@ class GeneralQSE(object):
         all_stdev = np.full(dim, np.nan)
         all_features = np.full(dim, np.nan)
 
-        # iterate over the bandwidths, make local polynom regression and caluclate the confidence intervalls
+        # iterate over the bandwidths, make local polynom regression and calculate the confidence intervals
         for j, h in enumerate(hs):
             print('calculate bandwidth... ', h)
-            features, stdev = self.local_polynom_regression(signal, h, min_stdev=stdev_est)
+            features, stdev, std_res = self.local_polynom_regression(signal, h, min_stdev=stdev_est)
             feature_0 = features[:, 0]
-            upper[:, j] = feature_0 + ici_span * stdev_est
-            lower[:, j] = feature_0 - ici_span * stdev_est
+            upper[:, j] = feature_0 + ici_span * std_res
+            lower[:, j] = feature_0 - ici_span * std_res
             all_stdev[:, :, j] = stdev
             all_features[:, :, j] = features
 
@@ -707,12 +725,13 @@ class GeneralQSE(object):
 
         # extract best index, attention: if all false then argmax gives first index but should take last index
         best_idx = np.argmax(criteria_mask, axis=1)
-        best_idx = np.array([nh - 1 if idx == 0 and not criteria_mask[j, idx] else idx for j, idx in enumerate(best_idx)])
+        best_idx = np.array([nh-1 if idx == 0 and not criteria_mask[j, idx] else idx for j, idx in enumerate(best_idx)])
 
         # smooth the choosen bandwidth index to avoid unnecessary jumps
         smoothed_idx = pd.Series(best_idx).rolling(self.ici_window, center=True).mean()
         smoothed_idx = smoothed_idx.fillna(method='ffill')
         smoothed_idx = smoothed_idx.fillna(method='bfill').values.astype(int)
+        smoothed_idx = best_idx
 
         # select the features and standard deviation of the chosen bandwidth
         nx_arange = np.arange(nx)
@@ -742,7 +761,7 @@ class GeneralQSE(object):
         # extract features (polynomial coefficients) and std_deviation dependend on the bandwidth estimation
         if self.bw_estimation == 'fix':
             stdev_est = stdev_estimation(signal)
-            all_features, all_stdev = self.local_polynom_regression(signal, bw_init, min_stdev=stdev_est)
+            all_features, all_stdev, _ = self.local_polynom_regression(signal, bw_init, min_stdev=stdev_est)
 
         elif self.bw_estimation == 'gcv':
             # methods: TNC, SLSQP, Nelder-Mead, COBYLA, L-BFGS-B
@@ -774,13 +793,13 @@ class GeneralQSE(object):
             best_bw = hs[np.argmin(maxgcv)]
             print('best bandwidth found: ', best_bw)
 
-            all_features, all_stdev = self.local_polynom_regression(signal, best_bw, stdev_est)
+            all_features, all_stdev, _ = self.local_polynom_regression(signal, best_bw, stdev_est)
 
         elif self.bw_estimation == 'ici':
             # featrues and stddev of variable bandwidth by applying the relative intersection of confidence intervals (RICI)
             all_features, all_stdev, best_bw = self.ici_method(signal)
 
-        # calculate the probabilieties out of features and stdev
+        # calculate the probabilities out of features and stdev
         all_primitive_prob = self.infer_probabilities(all_features, all_stdev)
 
         # make hidden markov model by iteration over each timestep

trend_analysis/qse_evaluation.py
@@ -7,9 +7,9 @@ from trend_analysis.qse_engine import GeneralQSE
 from trend_analysis.qse_utils import square_diff, cosine_diff, cross_corr, cross_entropy, classification_error
 
 
-def plot_qse(res, res_gt, coeff_nr=3, prim_nr=4, prim=['Flat', 'Down', 'Zero', 'Up'], save_path=None, text=None):
+def plot_qse(res, res_gt, coeff_nr, prim_nr, prim, save_path=None, text=None):
     offset = 2 * coeff_nr + 1
-    state_range = np.arange(offset + prim_nr, offset + 2 * prim_nr)
+    state_ranges = [np.arange(offset + prim_nr[0], offset + 2 * prim_nr[0]), np.arange(offset + prim_nr[1], offset + 2 * prim_nr[1])]
     nr = np.arange(res.shape[0])
     subplot_nr = 3
     colors = ['C0', 'C1', 'C2', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8', 'C9']
@@ -26,6 +26,8 @@ def plot_qse(res, res_gt, coeff_nr=3, prim_nr=4, prim=['Flat', 'Down', 'Zero', '
     l3 = ax2.plot(nr, res_gt[:, 0], 'r--', label='reference signal')
     ax2.set_ylabel('level [cm]', color='r')
     ax2.tick_params('y', colors='r')
+    ax3 = ax2.twinx()
+    ax3.plot(nr, res[:, -1])
 
     # add legend
     lns = l1 + l2 + l3
@@ -39,7 +41,7 @@ def plot_qse(res, res_gt, coeff_nr=3, prim_nr=4, prim=['Flat', 'Down', 'Zero', '
     for i, r in enumerate([res, res_gt]):
         name = 'reference' if i==1 else 'SOFI'
         axi = ax[i+1]
-        axi.stackplot(nr, *r[:, state_range].T, labels=prim)
+        axi.stackplot(nr, *r[:, state_ranges[i]].T, labels=prim[i])
         axi.set_ylabel(name +' prob. [-]')
         axi.set_yticks(np.arange(0, 1.5, step=0.5))
         axi.legend(loc='lower right')
@@ -63,33 +65,29 @@ def plot_qse(res, res_gt, coeff_nr=3, prim_nr=4, prim=['Flat', 'Down', 'Zero', '
     # ax[prim_nr].set_xlabel('Sample index [-]')
 
 
-def plot_presentation(y_pred, y_truth, store=None, delta=0.1, bw_ref=40):
-
-    # setup and initialization with tunning parameters
-    epsi = 0.000001
-    stay = 1  # how much more probable to stay in same primitive
-
-    trans = [['F', 'F', 0.33 * stay], ['F', 'L', 0.33],        ['F', 'U', 0.33],
-             ['L', 'F', 0.50],        ['L', 'L', 0.50 * stay], ['L', 'U', epsi],
-             ['U', 'F', 0.50],        ['U', 'L', epsi],        ['U', 'U', 0.50 * stay]]
+def ref_pred_comparison(y_pred, y_truth, p, store=None, bw_ref=10):
 
     bw_opt_sens = dict(n_support=bw_ref, min_support=40, max_support=400, ici_span=4.4, rel_threshold=0.85, irls=False)
-    bw_opt_sofi = dict(n_support=200, min_support=40, max_support=400, ici_span=4.4, rel_threshold=0.85, irls=True)
+    qse_sens = GeneralQSE(kernel='tricube', order=3, delta=0.05, transitions=p['trans'], bw_estimation='fix',
+                          bw_options=bw_opt_sens)
 
-    qse_sens = GeneralQSE(kernel='tricube', order=3, delta=0.05, transitions=trans, bw_estimation='fix', bw_options=bw_opt_sens)
-    qse_sofi = GeneralQSE(kernel='tricube', order=3, delta=delta, transitions=trans, bw_estimation='fix', bw_options=bw_opt_sofi)
+    bw_opt_sofi = dict(n_support=p['bw'], min_support=p['min_sup'], max_support=p['max_sup'],
+                       ici_span=p['ici'], rel_threshold=p['rel_th'], irls=p['irls'])
+    qse_sofi = GeneralQSE(kernel='tricube', order=3, sigma_eps=p['sig_e'], delta=p['delta'], transitions=p['trans'],
+                          bw_estimation=p['bw_est'], bw_options=bw_opt_sofi)
 
     # run algorithms
     res_sofi = qse_sofi.run(y_pred)
     res_sens = qse_sens.run(y_truth)
 
     # calculate difference
-    coeff_nr = 3
-    prim_nr = 4
+    coeff_nr = qse_sofi.coeff_nr
+    prim_nrs = [qse_sofi.prim_nr, qse_sens.prim_nr]
+    prims = [qse_sofi.primitives, qse_sens.primitives]
     sig_sens = res_sens[:, 1]
     sig_sofi = res_sofi[:, 1]
-    feat_sens = res_sens[:, 1 + 2 * coeff_nr + prim_nr:2 * coeff_nr + 2 * prim_nr + 1]
-    feat_sofi = res_sofi[:, 1 + 2 * coeff_nr + prim_nr:2 * coeff_nr + 2 * prim_nr + 1]
+    feat_sens = res_sens[:, 1 + 2 * coeff_nr + prim_nrs[1]:2 * coeff_nr + 2 * prim_nrs[1] + 1]
+    feat_sofi = res_sofi[:, 1 + 2 * coeff_nr + prim_nrs[0]:2 * coeff_nr + 2 * prim_nrs[0] + 1]
 
     # ccor = cross_corr(sig_sofi, sig_sens, col=1)
     ce = cross_entropy(feat_sofi, feat_sens)
@@ -100,28 +98,19 @@ def plot_presentation(y_pred, y_truth, store=None, delta=0.1, bw_ref=40):
 
     # plot results
     textstr = 'cross entropy$=%.2f$\naccuracy$=%.2f$' % (ce, ac)
-    plot_qse(res_sofi, res_sens, text=textstr, save_path=store)
+    # qse_sens.plot(res_sens)
+    # qse_sofi.plot(res_sofi)
+    plot_qse(res_sofi, res_sens, coeff_nr, prim_nrs, prims, text=textstr, save_path=store)
 
-    print('finish')
+    return ce, ac
+
 
 if __name__ == '__main__':
-    # path = "C:\\Users\\kramersi\\polybox\\4.Semester\\Master_Thesis\\02_QualitativeTrendAnalysis\\data"  # windows
-    # path = "/Users/simonkramer/Documents/Polybox/4.Semester/Master_Thesis/02_QualitativeTrendAnanalyis/data"  # mac
-    # path = "C:\\Users\\kramersi\\polybox\\4.Semester\\Master_Thesis\\02_QualitativeTrendAnalysis\\data"  # windows
-    # # path = "/Users/simonkramer/Documents/Polybox/4.Semester/Master_Thesis/02_QualitativeTrendAnanalyis/data"  # mac
-    #
-    # file_name = 'cam1_intra_0_0.2_0.4__ly4ftr16w2__cam1_0_0.2_0.4.csv'
-    #
-    # file_path = os.path.join(path, file_name)
-    #
-    # # load data from csv
-    # df = pd.read_csv(file_path, sep=',', dtype={'sensor_value': np.float64})
-    # df = df.interpolate()
-    # y_sofi = df['flood_index'].values
-    # y_sens = df['sensor_value'].values
-    path = "C:\\Users\\kramersi\\polybox\\4.Semester\\Master_Thesis\\03_ImageSegmentation\\structure_vidFloodExt\\signal"  # windows
+    path = "/Users/simonkramer/Documents/Polybox/4.Semester/Master_Thesis/03_ImageSegmentation/structure_vidFloodExt/signal"  # mac
+    #path = "C:\\Users\\kramersi\\polybox\\4.Semester\\Master_Thesis\\03_ImageSegmentation\\structure_vidFloodExt\\signal"  # windows
 
-    files = ['ft_l5b3e200f16_dr075i2res_lr__FloodX_cam1__signal.csv',
+    files = ['cam1_intra_0_0.2_0.4__ly4ftr16w2__cam1_0_0.2_0.4.csv',
+             'ft_l5b3e200f16_dr075i2res_lr__FloodX_cam1__signal.csv',
              'ft_l5b3e200f16_dr075i2res_lr__FloodX_cam5__signal.csv',
              'ft_l5b3e200f16_dr075i2res_lr__HoustonHarveyGarage__signal.csv',
              'ft_l5b3e200f16_dr075i2res_lr__ChaskaAthleticPark__signal.csv',
@@ -135,21 +124,70 @@ if __name__ == '__main__':
              'ref_l5b3e200f16_dr075i2res_lr__ChaskaAthleticPark__signal.csv',
              ]
 
-    zero_shift = [0, 0, 0.1, 0,    0.15, 0.14, 0.07, 0,        0.15, 0.27, 0.12, 0]
-    delta = [0.5, 0.3, 0.1, 0.1,      0.5, 0.3, 0.1, 0.1,     0.5, 0.3, 0.1, 0.1]
-    bw_ref = [80, 80, 20, 40,         80, 80, 20, 40,         80, 80, 20, 40]
+    # zero_shift = [0, 0, 0.1, 0,    0.15, 0.14, 0.07, 0,        0.15, 0.27, 0.12, 0]
+    # delta = [0.5, 0.3, 0.1, 0.1,      0.5, 0.3, 0.1, 0.1,     0.5, 0.3, 0.1, 0.1]
+    # bw_ref = [80, 80, 20, 40,         80, 80, 20, 40,         80, 80, 20, 40]
 
-    for i, file_name in enumerate(files):
-        if i in [1]:
+    # setup and initialization with tunning parameters
+    epsi = 0.000001
+
+    transLU = [['L', 'L', 0.5], ['L', 'U', 0.5],
+               ['U', 'L', 0.5], ['U', 'U', 0.5], ['F', 'F', 0.5]]
+
+    transLUF = [['F', 'F', 0.33], ['F', 'L', 0.33], ['F', 'U', 0.33],
+                ['L', 'F', 0.5], ['L', 'L', 0.5], ['L', 'U', epsi],
+                ['U', 'F', 0.5], ['U', 'L', epsi], ['U', 'U', 0.5]]
+
+    stay = 1  # how much more probable to stay in same primitive
+    transLUF_stay = [['F', 'F', 0.33 * stay], ['F', 'L', 0.33], ['F', 'U', 0.33],
+                     ['L', 'F', 0.50], ['L', 'L', 0.50 * stay], ['L', 'U', epsi],
+                     ['U', 'F', 0.50], ['U', 'L', epsi], ['U', 'U', 0.50 * stay]]
+
+
+    trans = [['Q0', 'Q0', 0.50 * stay], ['Q0', 'L', epsi], ['Q0', 'U', 0.50], ['Q0', 'F+', epsi],
+             ['L', 'Q0', 0.33], ['L', 'L', 0.33 * stay], ['L', 'U', epsi], ['L', 'F+', 0.33],
+             ['U', 'Q0', epsi], ['U', 'L', epsi], ['U', 'U', 0.50 * stay], ['U', 'F+', 0.50],
+             ['F+', 'Q0', epsi], ['F+', 'L', 0.33], ['F+', 'U', 0.33], ['F+', 'F+', 0.33 * stay]]
+
+    # sc0: tool just up and down and no signal smoothing, standard used in paper without smoothing
+    # sc1: + smoothing
+    # sc2: + primitive flat + delta
+    # sc3: + sigma epsilon est
+    # sc4: + adaptive bandwidht
+    # sc5: + irls
+    # sc6: + stay change in markov state
+    params = {
+        'sc0': dict(bw=3, min_sup=1, max_sup=1, ici=4.4, rel_th=0.85, irls=False, delta=0.0, bw_est='fix',
+                    trans=transLU, sig_e=0.001),
+        'sc1': dict(bw=200, min_sup=1, max_sup=1, ici=4.4, rel_th=0.85, irls=False, delta=0.0, bw_est='fix',
+                    trans=transLU, sig_e=0.001),
+        'sc2': dict(bw=200, min_sup=1, max_sup=1, ici=4.4, rel_th=0.85, irls=False, delta=0.05, bw_est='fix',
+                    trans=transLUF, sig_e=0.001),
+        'sc3': dict(bw=200, min_sup=1, max_sup=1, ici=4.4, rel_th=0.85, irls=False, delta=0.07, bw_est='fix',
+                    trans=transLUF, sig_e='auto'),
+        'sc4': dict(bw=200, min_sup=60, max_sup=400, ici=0.2, rel_th=None, irls=False, delta=[0.16, 0.05, 1], bw_est='fix',
+                    trans=trans, sig_e='auto'),
+        'sc5': dict(bw=200, min_sup=60, max_sup=400, ici=4.4, rel_th=0.85, irls=True, delta=0.07, bw_est='ici',
+                    trans=transLUF, sig_e='auto'),
+        'sc6': dict(bw=200, min_sup=60, max_sup=400, ici=4.4, rel_th=0.85, irls=True, delta=0.07, bw_est='ici',
+                    trans=transLUF_stay, sig_e='auto')
+    }
+
+    for i, file_name in enumerate(files):  # loop through files
+        if i in [0]:
             file_path = os.path.join(path, file_name)
-            store_name = file_name[:-10] + 'trend.png'
-            store_path = os.path.join(path, store_name)
 
             # load data from csv
             df = pd.read_csv(file_path, sep=',', dtype={'reference level': np.float64})
             df = df.interpolate()
-            y_sofi = df['extracted sofi'].values - zero_shift[i]
+            y_sofi = df['extracted sofi'].values
             y_sens = df['reference level'].values
 
-            # trend analyis of prediction and reference and calculate differences and plot
-            plot_presentation(y_sofi, y_sens, store=store_path, delta=delta[i], bw_ref=bw_ref[i])
+            for sc in params:  # loop through scenarios
+                if sc in ['sc4']:  # ['sc0', 'sc1', 'sc2', 'sc3', 'sc4', 'sc5', 'sc6']:
+                    # define figure name
+                    store_name = file_name[:-10] + 'trend_' + sc + '.png'
+                    store_path = os.path.join(path, store_name)
+
+                    # trend analyis of prediction and reference and calculate differences and plot
+                    ref_pred_comparison(y_sofi, y_sens, params[sc], store=store_path, bw_ref=40)